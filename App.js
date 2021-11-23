6497f7824f1d32683aa26876d90b89e4d788cf12
Richard Shea <rshea@thecubagroup.com>
2021-11-23 17:34:37 +1300

    End of lesson 8. Example of TouchableOpacity and FlatView

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList} from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('Richard Shea');
@@ -17,15 +17,24 @@ export default function App() {
       {name: 'Charlie', id: 9},
   ]);
 
+  const pressHandler = (id) => {
+      console.log(id);
+      setPeople((prevPeople) => {
+          return prevPeople.filter(person => person.id != id)
+      })
+  }
+
   return (
     <View style={styles.container}>
         <FlatList
-            key={999999}
+            key={999998}
             numColumns={2}
             keyExtractor={(item) => item.id}
             data={people}
             renderItem={({item}) => (
-                <Text style={styles.item}>{item.name}</Text>
+                <TouchableOpacity onPress={ () => pressHandler(item.id) }>
+                    <Text style={styles.item}>{item.name}</Text>
+                </TouchableOpacity>
             )}
         />
         {/*
