d68d54311d51367e79567f1e3e15b420c13f2cef
Richard Shea <rshea@thecubagroup.com>
2021-11-24 15:31:21 +1300

    After lesson #9

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header  from './components/Header';
 
 export default function App() {
 
+  const [todos, setTodos] = useState([
+    { text: 'buy tea', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ]);
+
   return (
     <View style={styles.container}>
-
+      <Header />
+      {/* header */}
+      <View style={styles.content}>
+      {/* Todo form */}
+        <View style={styles.list}>
+          <FlatList 
+						data={todos}
+						renderItem={({ item }) => (
+							<Text>{item.text}</Text>
+						)}
+          />
+        </View>
+      </View>
     </View>
   )
 }
@@ -15,4 +34,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+	content: {
+		padding: 40,
+	},
+	list: {
+		marginTop: 20,
+	},
 });
---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header() {
+
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>My Todos</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    backgroundColor: 'coral',
+    paddingTop: 38,
+  },
+  title: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+	},
+});
